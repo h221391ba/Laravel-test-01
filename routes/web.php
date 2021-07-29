4d370955a66821bb64cc58fe782a6d1b63bfa8f6
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-07-29 23:34:28 +0800

    delete everything

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductListController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,28 +14,5 @@ use App\Http\Controllers\ProductListController;
 */
 
 
-//攜帶參數的方法
-// Route::get('/{name}', function ($name) {
-//     return view('welcome', ['name' => $name]);
-// });
+Route::view('/', 'hello');
 
-//轉到其他路由
-// Route::get('/', function () {
-//     return view('welcome');
-
-
-    // 導去指定頁面
-    // return redirect('contact');
-// });
-
-//路由切換頁面的方式 完整寫法
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-//路由切換頁面的方式 簡易寫法 ('url', 'file name')
-Route::view('/hello', 'hello');
-
-Route::get('/', [ProductListController::class, 'index']);
-
-// Route::get('/productList', 'App\Http\Controllers\ProductListController@index');
