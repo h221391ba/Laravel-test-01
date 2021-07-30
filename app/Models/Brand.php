e5c87f68b614319cb6fca7f99dcd6d51621dd08f
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-07-30 18:37:03 +0800

    create model

---
+++ app/Models/Brand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function product(){
+        return $this->hasMany('App\Models\product');
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo('App\Models\Brand');
+    }
+}
